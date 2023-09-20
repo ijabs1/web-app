36e64cfe6f0f5f3ea7b28ca96053f5f7d95662be
Asaolu Olalekan <asaoluolalekan1@gmail.com>
2023-09-20 12:44:17 +0100

    Php app to build

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-
-FROM php:7.4-cli
-COPY . /usr/src/task2/php-docker
-WORKDIR /usr/src/task2/php-docker
-CMD [ "php", "./index.php" ] 
--- src/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<a href='/server_connection.php'>test connection</a><br>";
-echo "<a href='/registration.php'>Register</a>";
-
-?>
